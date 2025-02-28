a5b971c517dfe00bce0b67b8ef31eed305956b98
Erick Rodriguez <145071761+eriayay@users.noreply.github.com>
2025-02-28 16:57:51 -0500

    Update full_adder.rb

--- Proyecto 2/Chips/full_adder.rb
+++ Proyecto 2/Chips/full_adder.rb
@@ -7,7 +7,7 @@ end
 def full_adder(a, b, c)  
   ha1 = half_adder(a, b)
   ha2 = half_adder(ha1[:sum], c)
-  carry = ha1[:carry] | ha2[:carry] # OR operation
+  carry = ha1[:carry] | ha2[:carry] # OR
   { sum: ha2[:sum], carry: carry }
 end
 
